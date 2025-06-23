121977a55984deb3f8c4cf2fabbe9a711094eb4e
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-23 15:47:52 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -12,20 +11,25 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://quickzy-frontend.vercel.app"
+];
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://quickzy-frontend.onrender.com"
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
-
 
 app.use("/", userRoutes);
 app.use("/products", productRoutes);
