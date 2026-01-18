74f91d2d9a26bdf58b084b46722ca7ee71439dcc
ayushudaydubey <102001ayushdubey@gmail.com>
2026-01-18 23:28:33 +0530

    payment checout 1

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(passport.session());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://quickzy-real-2026.vercel.app/s"
+  "https://quickzy-real-2026.vercel.app/"
 ];
 
 app.use(cors({
